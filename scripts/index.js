f79ed7efb20b7fdbdea70369136b39019e06cdf1
Iona Avor <ionaavor@ionas-mbp.mynetworksettings.com>
2024-09-15 18:05:47 -0400

    New updates

--- scripts/index.js
+++ scripts/index.js
@@ -10,23 +10,23 @@ const initialCards = [
   },
 
   {
-    title: "Bald Mountains"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+    title: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   }
 
   {
-    title: "Latemar"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+    title: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   }
 
   {
-    title: "Vanoise National Park"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+    title: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   }
 
   {
-    title: "Lago di Braies"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+    title: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   }
 ];
 
