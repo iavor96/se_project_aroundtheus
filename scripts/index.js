2161321e9ec9cf3e9d5e22f6e52628b4a1b4960d
Iona Avor <ionaavor@ionas-mbp.mynetworksettings.com>
2024-09-15 18:07:24 -0400

    New updates

--- scripts/index.js
+++ scripts/index.js
@@ -12,22 +12,22 @@ const initialCards = [
   {
     title: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  }
+  },
 
   {
     title: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  }
+  },
 
   {
     title: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  }
+  },
 
   {
     title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  }
+  },
 ];
 
 console.log(cardData);
